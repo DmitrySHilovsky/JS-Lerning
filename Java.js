f44cafd6f2ada9550f5797eb516cbb7c5d3dda34
SHilovskyDmitry <wpbh@yandex.ru>
2023-01-21 21:35:02 +0300

    create type data

--- Java.js
+++ Java.js
@@ -1,14 +1,35 @@
+const test = (outputData, message = "") => {
+    console.log(outputData, ` >>>>> ${message} <<<<<`);    
+}
 // let a = 10;
 // a="sdfsdf";
 // console.log(a); //hoisting - возможность поднимать определения переменных вверх листинга кода
 
-const g = (a,b) =>a+b;  // вызов функции
+// const g = (a,b) => a+b;  // вызов функции фигурные скобки ненужны, функция сама добавит ретурн
 
-g(10,5);
+// g(10,5);
 
-const h = (a,b) => {
-    return a+b;
+// const h = (a,b) => {    // вызов функции несколько строк
+//     return a+b;
+// }
+
+// console.log(g(10,5)," Function G"); 
+// console.log(h(7,10)," Function H");
+
+// xxx = "строка";   // строка "", '', ``
+
+// console.log(`${xxx} ewwef`);   // ${} - интерполяция
+
+// const zzz = 10; // int - число, целые и дробные
+
+// test(zzz, "показать ccc"); // вместо консоль лога
+
+test(typeof(0.2 * 0.2));
+
+const userData = {                    // обьект
+    id: 5,
+    name: "vano",
+    roles: ["admin", "regular"],
 }
 
-console.log(g(10,5)," Function G"); 
-console.log(h(7,10)," Function H");
+const massuha = [1,2,3,4,5];         // массив
