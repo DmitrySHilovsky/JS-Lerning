f18ce9a0e7c4aa41086923cb4b4e73d1eaa4fe47
SHilovskyDmitry <wpbh@yandex.ru>
2023-01-24 03:27:52 +0300

    function

--- java.js
+++ java.js
@@ -1,48 +1,25 @@
-// const test = (outputData, message = "") => {
-//     console.log(outputData, ` >>>>> ${message} <<<<<`);    
-// }
+var test = function (outputData, message) {
+    if (message === void 0) { message = ""; }
+    console.log(outputData, " >>>>> ".concat(message, " <<<<<"));
+};
 // let a = 10;
 // a="sdfsdf";
 // console.log(a); //hoisting - возможность поднимать определения переменных вверх листинга кода
-
 // const g = (a,b) => a+b;  // вызов функции фигурные скобки ненужны, функция сама добавит ретурн
-
 // g(10,5);
-
 // const h = (a,b) => {    // вызов функции несколько строк
 //     return a+b;
 // }
-
 // console.log(g(10,5)," Function G"); 
 // console.log(h(7,10)," Function H");
-
 // xxx = "строка";   // строка "", '', ``
-
 // console.log(`${xxx} ewwef`);   // ${} - интерполяция
-
 // const zzz = 10; // int - число, целые и дробные
-
 // test(zzz, "показать ccc"); // вместо консоль лога
-
-// test(typeof(0.2 * 0.2));
-
-// const userData = {                    // обьект
-//     id: 5,
-//     name: "vano",
-//     roles: ["admin", "regular"],
-// }
-
-// const massuha = [1,2,3,4,5];         // массив
-
-// let named: string = "petr sdfsd dsfsdf";
-// console.log(named[named.length - 1]);
-
-// const text = 'When \t\n you play a \t\n game of thrones you win or you die.';
-// console.log(text.trim().slice(5,15));
-
-const motto = (ass:string) => {
-    let mmmm:String;
-    mmmm = "10";
-    console.log(mmmm+ass);
-}
-motto("17");
+test(typeof (0.2 * 0.2));
+var userData = {
+    id: 5,
+    name: "vano",
+    roles: ["admin", "regular"]
+};
+var massuha = [1, 2, 3, 4, 5]; // массив
